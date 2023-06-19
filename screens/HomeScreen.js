7acec11a5307dff32d3a1f9969cac78f6c7ec5bc
Shinajr <lasisioluwashina@gmail.com>
2023-06-19 19:23:19 +0100

    Feat: Added a react native modal to the app

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,16 +12,22 @@ import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 import Header from "../components/Header";
 import DatePicker from "react-native-date-ranges";
+import Modal from "react-native-modal";
 
 const HomeScreen = () => {
-  //storing our selcted date inside a state when it's selected  using the useState hook
+  //storing our selected date inside a state when it's selected  using the useState hook
   const [selectedDates, setSelectedDates] = useState();
   // console.warn(selectedDates)
   //initializing states for the rooms, adults and children so it can be updated when the state changes
   const [rooms, setRooms] = useState(1);
   const [children, setChildren] = useState(0);
   const [adults, setAdults] = useState(2);
+  //intializing a state to check if the modal is visible or not when the state changes
+  const [isModalVisible, setModalVisible] = useState(false);
 
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
   //initializing navigation
   const navigation = useNavigation();
   useLayoutEffect(() => {
@@ -63,118 +69,390 @@ const HomeScreen = () => {
     );
   };
   return (
-    <View>
-      <Header />
-      <ScrollView>
+    <>
+      <View>
+        <Header />
+        <ScrollView>
+          <View
+            style={{
+              margin: 20,
+              borderColor: "#FFC40C",
+              borderWidth: 2,
+              borderRadius: 6,
+            }}
+          >
+            {/* Destination */}
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC40C",
+                borderWidth: 2,
+                paddingVertical: 15,
+              }}
+            >
+              <Ionicons name="search-sharp" size={24} color="black" />
+              <TextInput
+                placeholderTextColor="black"
+                placeholder="Enter your Destination"
+              />
+            </Pressable>
+            {/* Selected Dates */}
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC40C",
+                borderWidth: 2,
+                paddingVertical: 15,
+              }}
+            >
+              <Ionicons name="calendar-sharp" size={24} color="black" />
+              <DatePicker
+                style={{
+                  width: 350,
+                  height: 30,
+                  borderWidth: 0,
+                  borderColor: "transparent",
+                }}
+                customStyles={{
+                  placeholderText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  }, // placeHolder style
+                  headerStyle: {
+                    backgroundColor: "#2c3968",
+                  },
+                  contentText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                }}
+                selectedBgColor="#0047AB"
+                customButton={(onConfirm) => customButton(onConfirm)}
+                onConfirm={(startDate, endDate) =>
+                  setSelectedDates(startDate, endDate)
+                }
+                allowFontScaling={false} // optional
+                placeholder={"Jul 27, 2023 → Sep 10, 2023"}
+                mode={"range"}
+              />
+            </Pressable>
+            {/* Rooms and Guests */}
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC40C",
+                borderWidth: 2,
+                paddingVertical: 15,
+              }}
+              onPress={toggleModal}
+            >
+              <Ionicons name="ios-person-outline" size={24} color="black" />
+              <TextInput
+                placeholderTextColor="red"
+                placeholder="1 room * 2 adults * 0 children"
+              />
+            </Pressable>
+            {/* Search Button */}
+            <Pressable
+              style={{
+                paddingHorizontal: 10,
+                borderColor: "#FFC40C",
+                borderWidth: 2,
+                paddingVertical: 15,
+                backgroundColor: "#0047AB",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 15,
+                  fontWeight: "500",
+                  color: "white",
+                }}
+              >
+                Search
+              </Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </View>
+      <Modal
+        swipeThreshold={200}
+        swipeDirection={["up", "down"]}
+        isVisible={isModalVisible}
+        animationIn="slideInUp"
+        animationInTiming={2000}
+        animationOut="slideOutDown"
+        animationOutTiming={2000}
+        onBackdropPress={() => setModalVisible(false)}
+        onBackButtonPress={() => setModalVisible(false)}
+        style={{
+          flex: 1,
+          backgroundColor: "white",
+          marginTop: 430,
+          position: "relative",
+        }}
+      >
         <View
           style={{
-            margin: 20,
-            borderColor: "#FFC40C",
-            borderWidth: 2,
-            borderRadius: 6,
+            flex: 1,
+            backgroundColor: "white",
           }}
         >
-          {/* Destination */}
-          <Pressable
+          <View
             style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              paddingHorizontal: 10,
-              borderColor: "#FFC40C",
-              borderWidth: 2,
-              paddingVertical: 15,
-            }}
-          >
-            <Ionicons name="search-sharp" size={24} color="black" />
-            <TextInput placeholderTextColor="black" placeholder="Enter your Destination" />
-          </Pressable>
-          {/* Selected Dates */}
-          <Pressable
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              paddingHorizontal: 10,
-              borderColor: "#FFC40C",
-              borderWidth: 2,
-              paddingVertical: 15,
-            }}
-          >
-            <Ionicons name="calendar-sharp" size={24} color="black" />
-            <DatePicker
-              style={{
-                width: 350,
-                height: 30,
-                borderWidth: 0,
-                borderColor: "transparent",
-              }}
-              customStyles={{
-                placeholderText: {
-                  fontSize: 15,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginRight: "auto",
-                }, // placeHolder style
-                headerStyle: {
-                  backgroundColor: "#2c3968",
-                },
-                contentText: {
-                  fontSize: 15,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginRight: "auto",
-                },
-              }}
-              selectedBgColor="#0047AB"
-              customButton={(onConfirm) => customButton(onConfirm)}
-              onConfirm={(startDate, endDate) =>
-                setSelectedDates(startDate, endDate)
-              }
-              allowFontScaling={false} // optional
-              placeholder={"Jul 27, 2023 → Sep 10, 2023"}
-              mode={"range"}
-            />
-          </Pressable>
-          {/* Rooms and Guests */}
-          <Pressable
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              paddingHorizontal: 10,
-              borderColor: "#FFC40C",
-              borderWidth: 2,
-              paddingVertical: 15,
-            }}
-          >
-            <Ionicons name="ios-person-outline" size={24} color="black" />
-            <TextInput placeholderTextColor="red" placeholder="1 room * 2 adults * 0 children" />
-          </Pressable>
-          {/* Search Button */}
-          <Pressable
-            style={{
-              paddingHorizontal: 10,
-              borderColor: "#FFC40C",
-              borderWidth: 2,
-              paddingVertical: 15,
-              backgroundColor: "#0047AB",
+              backgroundColor: "white",
+              width: "100%",
+              padding: 4,
+              borderBottomWidth: 0.5,
+              borderBottomColor: "#000",
             }}
           >
             <Text
               style={{
+                color: "black",
+                fontSize: 15,
                 textAlign: "center",
-                fontSize: 17,
-                fontWeight: "500",
-                color: "white",
               }}
             >
-              Search
+              Select rooms and guests
             </Text>
-          </Pressable>
+          </View>
+          <View style={{ width: "90%", height: 310, marginHorizontal: 15 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginVertical: 15,
+              }}
+            >
+              <Text style={{ fontSize: 16, fontWeight: "500" }}>Rooms</Text>
+              <Pressable
+                style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+              >
+                <Pressable
+                  onPress={() => setRooms(Math.max(1, rooms - 1))}
+                  style={{
+                    width: 26,
+                    height: 26,
+                    borderRadius: 13,
+                    borderColor: "#BEBEBE",
+                    backgroundColor: "#E0E0E0",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 20,
+                      fontWeight: "600",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    -
+                  </Text>
+                </Pressable>
+                <Pressable>
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 18,
+                      fontWeight: "500",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    {rooms}
+                  </Text>
+                </Pressable>
+                <Pressable
+                onPress={()=> setRooms((c) => c+1)}
+                  style={{
+                    width: 26,
+                    height: 26,
+                    borderRadius: 13,
+                    borderColor: "#BEBEBE",
+                    backgroundColor: "#E0E0E0",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 20,
+                      fontWeight: "600",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    +
+                  </Text>
+                </Pressable>
+              </Pressable>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginVertical: 15,
+              }}
+            >
+              <Text style={{ fontSize: 16, fontWeight: "500" }}>Adults</Text>
+              <Pressable
+                style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+              >
+                <Pressable
+                onPress={()=> setAdults(Math.max(1, adults-1))}
+                  style={{
+                    width: 26,
+                    height: 26,
+                    borderRadius: 13,
+                    borderColor: "#BEBEBE",
+                    backgroundColor: "#E0E0E0",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 20,
+                      fontWeight: "600",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    -
+                  </Text>
+                </Pressable>
+                <Pressable>
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 18,
+                      fontWeight: "500",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    {adults}
+                  </Text>
+                </Pressable>
+                <Pressable
+                onPress={()=> setAdults((c) => c+1)}
+                  style={{
+                    width: 26,
+                    height: 26,
+                    borderRadius: 13,
+                    borderColor: "#BEBEBE",
+                    backgroundColor: "#E0E0E0",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 20,
+                      fontWeight: "600",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    +
+                  </Text>
+                </Pressable>
+              </Pressable>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginVertical: 15,
+              }}
+            >
+              <Text style={{ fontSize: 16, fontWeight: "500" }}>Children</Text>
+              <Pressable
+                style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+              >
+                <Pressable
+                onPress={()=> setChildren(Math.max(0, children-1))}
+                  style={{
+                    width: 26,
+                    height: 26,
+                    borderRadius: 13,
+                    borderColor: "#BEBEBE",
+                    backgroundColor: "#E0E0E0",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 20,
+                      fontWeight: "600",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    -
+                  </Text>
+                </Pressable>
+                <Pressable>
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 18,
+                      fontWeight: "500",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    {children}
+                  </Text>
+                </Pressable>
+                <Pressable
+                onPress={()=> setChildren((c) => c+1)}
+                  style={{
+                    width: 26,
+                    height: 26,
+                    borderRadius: 13,
+                    borderColor: "#BEBEBE",
+                    backgroundColor: "#E0E0E0",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 20,
+                      fontWeight: "600",
+                      paddingHorizontal: 6,
+                    }}
+                  >
+                    +
+                  </Text>
+                </Pressable>
+              </Pressable>
+            </View>
+          </View>
         </View>
-      </ScrollView>
-    </View>
+        {/* //The parent component must have the relative tag and button should have an absolute tag. */}
+        <Button
+          title="Hide modal"
+          onPress={toggleModal}
+          style={{ position: "relative", bottom: 0 }}
+          color="#0047AB"
+        />
+      </Modal>
+    </>
   );
 };
 
